9a249141ce68a91160a4a6fbb26a321761ea47fe
Rohan Yadav <rajrohanyadav@gmail.com>
2017-06-06 15:37:38 +0530

    minor changes in ui

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect("mongodb://localhost/blog_app");
 
 // App Config
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
